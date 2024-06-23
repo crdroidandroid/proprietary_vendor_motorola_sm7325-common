d33e25ba38bc84ca5f204bbba25fd5a0eeaacfd5
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    sm7325-common: Declare IMS libs as symlinks during extraction
    Change-Id: Ia1b8097d28dd4888232fe383b830515b1b9bafaa

--- sm7325-common-vendor.mk
+++ sm7325-common-vendor.mk
@@ -1123,3 +1123,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.diag.hal.service \
     vendor.qti.gnss@4.0-service \
     vendor.qti.hardware.servicetracker@1.2-service
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
